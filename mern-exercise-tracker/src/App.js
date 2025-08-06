6d43a5a75df8af96d9606a531512dec742d00c98
av <avinabo.tarafder@gmail.com>
2025-08-06 09:27:37 +0600

    components-initialize

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,12 +1,41 @@
-// filepath: mern-exercise-tracker/src/App.js
-import SignupForm from './SignupForm';
-import './App.css';
+// // filepath: mern-exercise-tracker/src/App.js
+// import SignupForm from './SignupForm';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <SignupForm />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <SignupForm />
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Routes>
+          <Route path="/" element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
